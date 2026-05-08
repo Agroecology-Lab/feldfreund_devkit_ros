8e419f9c5cf2aaa453a5f7e6aa6d84ceb3210887
samuk <sam@bristolwireless.net>
2026-05-08 11:18:43 +0100

    Delete devkit_driver directory

--- devkit_driver/devkit_driver/qos.py
+++
@@ -1,7 +0,0 @@
-from rclpy.qos import DurabilityPolicy, Duration, LivelinessPolicy, QoSProfile, ReliabilityPolicy
-
-SAFETY_QOS = QoSProfile(depth=1,
-                        reliability=ReliabilityPolicy.RELIABLE,
-                        durability=DurabilityPolicy.TRANSIENT_LOCAL,
-                        liveliness=LivelinessPolicy.AUTOMATIC,
-                        liveliness_lease_duration=Duration(seconds=1))
